24ffd60681407f37b780a91a34fa6c21c0912dfd
Vamsi Krishna Aitha <ivamsi2001@gmail.com>
2021-06-17 23:20:51 +0530

    Ingress Changes

---
+++ src/main/java/com/qualitto/user/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.qualitto.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+	@GetMapping("/health")
+	public String healthChk() {
+		return "User Service OK";
+	}
+}
--- src/main/java/com/qualitto/user/controller/UserController.java
+++ src/main/java/com/qualitto/user/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qualitto.user.VO.ResponseTemplate;
@@ -15,7 +14,6 @@ import com.qualitto.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/users")
 @Slf4j
 public class UserController {
 
